54ba2ea372f0a616c5666fe7f6e6488715917405
Paddy Cremen <cremenp@tcd.ie>
2018-05-25 13:30:13 +0000

    add functionality to add form values to RecipeIngredients table

--- app.py
+++ app.py
@@ -7,17 +7,16 @@ from werkzeug.utils import secure_filename #for uploading images
 
 
 app = Flask(__name__)
-"""
- INSERT INTO Recipes (Name, UserId, Difficulty, Serves, Blurb, PrepTime, CookTime, Instructions)
- VALUES ("Chicken Fillet Role", "123", "1", "4", "From spar. Good chicken fillet", "11:12:00", "04:10:00", "['First do this', 'Then do that', 'Then this']");
- """
-app.secret_key = 'some_secret'
 
+app.secret_key = 'some_secret'
 
 username = os.getenv("C9_USER")
 
-
 connection = pymysql.connect(host = 'localhost', user= username, password = "", db="milestoneProjectFour")
+
+"""
+HELPER FUNCTIONS
+"""
 
 
 def get_recipe_image():
@@ -25,7 +24,6 @@ def get_recipe_image():
     returns the image that the user uploads 
     returns false if user didn't upload image
     """
-    
     try:
         recipe_image = request.files["recipe-img"]
         image_added = True
@@ -38,10 +36,11 @@ def get_recipe_image():
         return False
         
     
-    
-    
 def get_prep_time():
-    # https://stackoverflow.com/questions/14295673/convert-string-into-datetime-time-object
+    """
+    returns prep time from the form in datetime format
+    code partly from:  https://stackoverflow.com/questions/14295673/convert-string-into-datetime-time-object
+    """
     prep_hours = request.form["prep-hours"]
     prep_mins = request.form["prep-mins"]
     prep_time =   datetime.datetime.strptime('{0}:{1}'.format(prep_hours, prep_mins), '%H:%M').time()
@@ -49,7 +48,10 @@ def get_prep_time():
     return prep_time
     
 def get_cook_time():
-    # https://stackoverflow.com/questions/14295673/convert-string-into-datetime-time-object
+    """
+    returns prep time from the form in datetime format
+    code partly from:  https://stackoverflow.com/questions/14295673/convert-string-into-datetime-time-object
+    """
     cook_hours = request.form["cook-hours"]
     cook_mins = request.form["cook-mins"]
     cook_time =   datetime.datetime.strptime('{0}:{1}'.format(cook_hours, cook_mins), '%H:%M').time()
@@ -58,7 +60,10 @@ def get_cook_time():
     
     
 def get_categories_list():
-    
+    """
+    returns a list of all categories entered 
+    into the form
+    """
     end_of_categories = False
     categories_list = []
     counter = 0
@@ -73,6 +78,10 @@ def get_categories_list():
     return categories_list
     
 def get_instructions_list():
+    """
+    returns a list of all instructions 
+    entered into the form
+    """
     
     end_of_instructions = False
     instructions_list = []
@@ -89,6 +98,11 @@ def get_instructions_list():
     return instructions_list
     
 def get_ingredients_dictionary_list():
+    """
+    returns a list of dictionaries. Each dictionary
+    represents an ingredient entered into the form with
+    'Quantity' and 'Name" as keys
+    """
     
     end_of_ingredients = False
     ingredients_dictionary_list = []
@@ -107,80 +121,7 @@ def get_ingredients_dictionary_list():
         counter+=1
         
     return ingredients_dictionary_list
-        
-        
-# def get_categories():
-#     # https://blog.miguelgrinberg.com/post/easy-web-scraping-with-python/page/2
     
-#     url= "https://milestone-project-4-paddywc.c9users.io/categories"
-    
-#     response = requests.get(url)
-#     soup =  bs4.BeautifulSoup(response.text, "lxml")
-#     # categories = [i.get_text() for i in soup.select("div.chips")]
-#     categories  = soup.select("div.row")
-    
-#     print(categories)
-#     return categories 
-        
-    
-
-def get_form_values():
-
-    values_dictionary = {
-        "Name" : request.form["recipe-name"],
-        "Image": get_recipe_image(),
-        "Difficulty": request.form["difficulty-select"],
-        "Serves" : request.form["serves"],
-        "Blurb" : request.form["blurb"],
-        "PrepTime": get_prep_time(),
-        "CookTime": get_cook_time(),
-        "Instructions": get_instructions_list(),
-        "Categories" : get_categories_list(),
-        "Ingredients" : get_ingredients_dictionary_list()
-        
-    }
-    print(values_dictionary["Ingredients"])
-    return values_dictionary
-    
-def add_to_categories_if_not_duplicate(category_list):
-    # https://stackoverflow.com/questions/3164505/mysql-insert-record-if-not-exists-in-table
-    try:
-        with connection.cursor() as cursor:
-            for category in category_list:
-                lower_case = category.lower()
-                capitalized = category.capitalize()
-                cursor.execute('INSERT INTO Categories(Name) SELECT * FROM (SELECT "{0}" ) AS tmp WHERE NOT EXISTS (SELECT Name FROM Categories WHERE Name = "{0}");'.format(capitalized))
-            connection.commit()
-        
-    except Exception as e:
-        print(e)
-        connection.close()    
-        
-def add_to_ingredients_if_not_duplicate(ingredients_dictionary_list):
-    
-    
-    ingredients_name_list = [ingredient_dictionary["Name"] for ingredient_dictionary in ingredients_dictionary_list ]
-    try:
-        with connection.cursor() as cursor:
-            for ingredient_name in ingredients_name_list:
-                cursor.execute('INSERT INTO Ingredients(Name) SELECT * FROM (SELECT "{0}" ) AS tmp WHERE NOT EXISTS (SELECT Name FROM Ingredients WHERE Name = "{0}");'.format(ingredient_name))
-            connection.commit()
-    except Exception as e:
-        print(e)
-        connection.close()
-            
- 
-
-def test_function():
-    try:
-        with connection.cursor() as cursor:
-            cursor.execute("SET FOREIGN_KEY_CHECKS=0")
-            insert_into = "(Name, UserId, Difficulty, Serves, Blurb, PrepTime, CookTime, Instructions)"
-            values = '("Chicken Fillet Role", "123", "1", "4", "From spar. Good chicken fillet", "11:12:00", "04:10:00", "{}")'.format("['First do this', 'Then do that', 'Then this']")
-            cursor.execute("INSERT INTO Recipes{0} VALUES {1};".format(insert_into, values))
-            connection.commit()
-    finally:
-        connection.close()
         
         
 def create_recipe_values_without_image(values_dictionary):
@@ -218,6 +159,129 @@ def create_recipe_values_with_image(values_dictionary):
     
     return values
     
+    
+
+""" 
+OTHER FUNCTIONS
+"""
+        
+        
+def get_form_values():
+    """
+    Returns a dictionary with all values entered into
+    the add recipe form. Keys match their SQL names
+    """
+
+    values_dictionary = {
+        "Name" : request.form["recipe-name"],
+        "Image": get_recipe_image(),
+        "Difficulty": request.form["difficulty-select"],
+        "Serves" : request.form["serves"],
+        "Blurb" : request.form["blurb"],
+        "PrepTime": get_prep_time(),
+        "CookTime": get_cook_time(),
+        "Instructions": get_instructions_list(),
+        "Categories" : get_categories_list(),
+        "Ingredients" : get_ingredients_dictionary_list()
+        
+    }
+    # print(values_dictionary["Ingredients"])
+    return values_dictionary
+    
+"""
+INTERACTING WITH MYSQL
+"""
+
+
+def get_last_recipe_id():
+    """
+    returns the value of the last Id created in
+    MySQL
+    """
+    
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT LAST_INSERT_ID()")
+            last_id_tuple = cursor.fetchone()
+            last_id = last_id_tuple[0]
+            return last_id
+    except Exception as e:
+       print("ERROR GET LAST ID: {}".format(e))
+       
+   
+    
+def add_to_categories_if_not_duplicate(category_list):
+    """
+    adds each category to Categories SQL table if 
+    category name does not already exist in table
+    code from: https://stackoverflow.com/questions/3164505/mysql-insert-record-if-not-exists-in-table
+    """
+    print(category_list)
+    try:
+        with connection.cursor() as cursor:
+            for category in category_list:
+                lower_case = category.lower()
+                capitalized = category.capitalize()
+                cursor.execute('INSERT INTO Categories(Name) SELECT * FROM (SELECT "{0}" ) AS tmp WHERE NOT EXISTS (SELECT Name FROM Categories WHERE Name = "{0}");'.format(capitalized))
+            connection.commit()
+        
+    except Exception as e:
+        print("ERROR ADD TO CATAGORIES: {}".format(e))
+        # connection.close()    
+        
+def add_to_ingredients_if_not_duplicate(ingredients_dictionary_list):
+    """
+    adds each ingredient name to Ingredients SQL table if 
+    ingredient name does not already exist in table
+    code from: https://stackoverflow.com/questions/3164505/mysql-insert-record-if-not-exists-in-table
+    """
+    
+    ingredients_name_list = [ingredient_dictionary["Name"] for ingredient_dictionary in ingredients_dictionary_list ]
+    try:
+        with connection.cursor() as cursor:
+            for ingredient_name in ingredients_name_list:
+                cursor.execute('INSERT INTO Ingredients(Name) SELECT * FROM (SELECT "{0}" ) AS tmp WHERE NOT EXISTS (SELECT Name FROM Ingredients WHERE Name = "{0}");'.format(ingredient_name))
+            connection.commit()
+    except Exception as e:
+        print("ERROR ADD TO INGREDIENTS: {}".format(e))
+        # connection.close()
+            
+def add_to_recipe_ingredients(ingredients_dictionary_list, recipe_id):
+
+    
+    try:
+        with connection.cursor() as cursor:
+            for ingredient_dictionary in ingredients_dictionary_list:
+                cursor.execute('INSERT INTO RecipeIngredients(RecipeId, IngredientId, Quantity) VALUES ("{0}", (SELECT Id FROM Ingredients WHERE Name="{1}"), "{2}")'.format(
+                    recipe_id,
+                    ingredient_dictionary["Name"],
+                    ingredient_dictionary["Quantity"]
+                    ))
+                    
+            connection.commit()
+    except Exception as e:
+        print("RECIPEINGRedients ERROR: {}".format(e))
+        # connection.close()
+    
+    
+    
+
+       
+
+    
+
+    
+# def test_function():
+#     try:
+#         with connection.cursor() as cursor:
+#             cursor.execute("SET FOREIGN_KEY_CHECKS=0")
+#             insert_into = "(Name, UserId, Difficulty, Serves, Blurb, PrepTime, CookTime, Instructions)"
+#             values = '("Chicken Fillet Role", "123", "1", "4", "From spar. Good chicken fillet", "11:12:00", "04:10:00", "{}")'.format("['First do this', 'Then do that', 'Then this']")
+#             cursor.execute("INSERT INTO Recipes{0} VALUES {1};".format(insert_into, values))
+#             connection.commit()
+#     finally:
+#         connection.close()
+
         
 def insert_dictionary_into_recipes_table(values_dictionary):
     """
@@ -225,16 +289,12 @@ def insert_dictionary_into_recipes_table(values_dictionary):
     form values into the MySQL database
     """
     
-    
-    
     if values_dictionary["Image"]:
         insert_into = "(Name, UserId, Image, Difficulty, Serves, Blurb, PrepTime, CookTime, Instructions)"
         values = create_recipe_values_with_image(values_dictionary)
     else:
         insert_into = "(Name, UserId, Difficulty, Serves, Blurb, PrepTime, CookTime, Instructions)"
         values = create_recipe_values_without_image(values_dictionary)
-    
-    
 
     try:
         with connection.cursor() as cursor:
@@ -242,26 +302,25 @@ def insert_dictionary_into_recipes_table(values_dictionary):
             cursor.execute("INSERT INTO Recipes{0} VALUES {1};".format(insert_into, values))
             connection.commit()
     except Exception as e:
-        print(e)
-        connection.close()
+        print("ERROR  INSERT into recipes: {0}".format(e))
+        # connection.close()
             
     
 @app.route("/",  methods=["POST", "GET"] )
 def add_recipe():
     if request.method == "POST":
         values_dictionary = get_form_values()
+        # print(values_dictionary)
         insert_dictionary_into_recipes_table(values_dictionary)
+        recipe_id = get_last_recipe_id()
         add_to_categories_if_not_duplicate(values_dictionary["Categories"])
         add_to_ingredients_if_not_duplicate(values_dictionary["Ingredients"])
+        add_to_recipe_ingredients(values_dictionary["Ingredients"], recipe_id)
         
         return render_template("addrecipe.html", testvalue="POST")
     
   
     return render_template("addrecipe.html", testvalue="NOT POST")
-    
-@app.route("/categories",  methods=["POST", "GET"] )
-def enter_categories():
-    return render_template("categories.html", testvalue="POST")
     
 
 
